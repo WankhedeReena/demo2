ad8063035b3f2bc1d6b0339eb0f69b3192105025
WankhedeReena <reena.wankhede174@gmail.com>
2021-05-03 18:34:33 +0530

    demo commit

---
+++ src/test/java/visionIT/FirstMavenProgram.java
@@ -0,0 +1,14 @@
+package visionIT;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstMavenProgram {
+	public static void main(String[] args) 
+	{
+
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\exes\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("www.google.com");
+	}
+}
